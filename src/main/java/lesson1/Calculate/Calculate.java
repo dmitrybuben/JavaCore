978c56c73e4c80ae4c6e8977f87f899fb5b685ec
dmitrybuben <dmitry.buben@gmail.com>
2023-10-30 20:16:02 +0300

    classes created

--- src/main/java/lesson1/Calculate/Calculate.java
+++ src/main/java/lesson1/Calculate/Calculate.java
@@ -1,2 +1,19 @@
-package lesson1.Calculate;public class Calculate {
+package lesson1.Calculate;
+
+public class Calculate {
+    public static int sum(int a, int b) {
+        return a+b;
+    }
+
+    public static int sub(int a, int b) {
+        return a-b;
+    }
+
+    public static int div(int a, int b) {
+        return a/b;
+    }
+
+    public static int mul(int a, int b) {
+        return a*b;
+    }
 }
--- src/main/java/lesson1/Execute/Main.java
+++ src/main/java/lesson1/Execute/Main.java
@@ -1,7 +1,16 @@
-package lesson1;
+package lesson1.Execute;
+
+import lesson1.Calculate.Calculate;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int res = Calculate.sum(5,5);
+        System.out.printf("The result is: %d.\n",res);
+        res = Calculate.sub(10,4);
+        System.out.printf("The result is: %d.\n",res);
+        res = Calculate.div(10,2);
+        System.out.printf("The result is: %d.\n",res);
+        res = Calculate.mul(10,2);
+        System.out.printf("The result is: %d.\n",res);
     }
 }
